4064105a4b63f5cd9895d8208c11d2b4d77340bc
nierowheezy <Olaniyiolabode99@yahoo.com>
2020-01-14 18:40:16 +0100

    made changes to models

--- olaniyiolabode99scrumy/models.py
+++ olaniyiolabode99scrumy/models.py
@@ -9,22 +9,24 @@ from django.contrib.auth.models import User
 
 
 class GoalStatus(models.Model):
-    status_name = models.TextField()
+    status_name = models.CharField(max_length=100)
 
 
 class ScrumyGoals(models.Model):
-    goal_name = models.TextField()
+    user = models.ForeignKey(User, related_name='goals',
+                             on_delete=models.CASCADE)
     goal_id = models.IntegerField()
-    created_by = models.TextField()
-    moved_by = models.TextField()
-    owner = models.TextField()
-    goal_status = models.ForeignKey('GoalStatus', on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    goal_name = models.CharField(max_length=100)
+    goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT)
+    created_by = models.CharField(max_length=100)
+    moved_by = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
 
 
 class ScrumyHistory(models.Model):
-    moved_by = models.TextField()
-    created_by = models.TextField()
-    moved_from = models.TextField()
-    moved_to = models.TextField()
-    time_of_action = models.TimeField()
+    goal = models.ForeignKey(ScrumyGoals, on_delete=models.PROTECT, default=0.0)
+    moved_by = models.CharField(max_length=100)
+    created_by = models.CharField(max_length=100)
+    moved_from = models.CharField(max_length=100)
+    moved_to = models.CharField(max_length=100)
+    time_of_action = models.DateField()
