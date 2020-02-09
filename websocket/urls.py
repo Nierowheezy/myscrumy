9b638475411e2d83573bae6d566a89a30ea24fc4
nierowheezy <Olaniyiolabode99@yahoo.com>
2020-02-09 16:43:12 +0100

    changes made to urls.py

--- websocket/urls.py
+++ websocket/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('test/', views.test, name='test'),
     path('connect/', views.connect, name='connect'),
     path('disconnect/', views.disconnect, name='disconnect'),
-    path('send_message/', views.send_message, name='send message'),
-    path('get_recent_messages/', views.get_recent_messages,
-         name='get recent messages')
+    path('send_message/', views.send_message, name='send message')
 ]
