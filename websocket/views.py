8e2318ec23ec98138d416169a8eb45ef34992b67
nierowheezy <Olaniyiolabode99@yahoo.com>
2020-02-09 11:22:48 +0100

    code

--- websocket/views.py
+++ websocket/views.py
@@ -1,28 +1,76 @@
 import json
-from .models import ChatMessage, Connection
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import *
+import boto3
 # Create your views here.
-@csrf_exempt
-def test(request):
-    return JsonResponse({'message': 'Hello Daud'}, status=200)
 
 
 def _parse_body(body):
     body_unicode = body.decode('utf-8')
+    print(body_unicode)
+    return json.loads(body)
+
+
+@csrf_exempt
+def test(request):
+    return JsonResponse({'message': 'hello Daud'}, status=200)
 
 
 @csrf_exempt
 def connect(request):
     body = _parse_body(request.body)
+    print(body)
     connection_id = body['connectionId']
-    Connection.objects.create(connection_id=connection_id)
-    return JsonResponse('connect successfully', status=200)
+    print(connection_id)
+    id = CreateConnection.objects.create(connection_id=connection_id)
+    id.save()
+    return JsonResponse({"message": "connected successfuly"}, status=200)
 
 
+@csrf_exempt
 def disconnect(request):
     body = _parse_body(request.body)
+    print(body)
     connection_id = body['connectionId']
-    Connection.objects.delete(connection_id=connection_id)
-    return JsonResponse('disconnect successfully', status=200)
+    print(connection_id)
+    id = CreateConnection.objects.get(connection_id=connection_id)
+    id.delete()
+    return JsonResponse("disconnected successfuly", status=200, safe=False)
+
+
+def _send_to_connection(connection_id, data):
+    gatewayapi = boto3.client(
+        'apigatewaymanagementapi',
+        endpoint_url='https://4hvqalbj8k.execute-api.us-east-2.amazonaws.com/test/',
+        region_name='us-east-1',
+        aws_access_key_id='AKIAJGJCIPJC6ABU3STA',
+        aws_secret_access_key='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'
+    )
+    return gatewayapi.post_to_connection(
+        ConnectionId=connection_id,
+        Data=json.dumps(data).encode('utf-8'))
+
+
+@csrf_exempt
+def send_message(request):
+    body = _parse_body(request.body)
+    chat = ChatMessage()
+    chat.username = body['username']
+    chat.message = body['message']
+    chat.timestamp = body['timestamp']
+    chat.save()
+    connections = CreateConnection.objects.all()
+    data = {'messages': [body]}
+    for connection in connections:
+        _send_to_connection(connection.connection_id, data)
+    return JsonResponse({"message": "successfully sent"}, status=200)
+
+
+@csrf_exempt
+def get_recent_messages(request):
+    body = _parse_body(request.body)
+    connections = ChatMessage.objects.all()
+    return JsonResponse({'messages':[{'username':connection.username, 'message':connection.message,
+    'timestamp':connection.timestamp} for connection in connections]}, status=200)
