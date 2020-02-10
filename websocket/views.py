4a9d67d431f05e1bb6b126853903b26416147119
nierowheezy <Olaniyiolabode99@yahoo.com>
2020-02-10 11:32:43 +0100

    af code test

--- websocket/views.py
+++ websocket/views.py
@@ -1,4 +1,3 @@
-
 import json
 from .models import ChatMessage, Connection
 from django.shortcuts import render
@@ -8,14 +7,11 @@ import boto3
 # Create your views here.
 @csrf_exempt
 def test(request):
-    return JsonResponse({'message': 'Hello Daud'}, status=200)
-
+    return JsonResponse({'message':'Hello Daud'}, status=200)
 
 def _parse_body(body):
     body_unicode = body.decode('utf-8')
     return json.loads(body_unicode)
-
-
 @csrf_exempt
 def connect(request):
     body = _parse_body(request.body)
@@ -23,12 +19,10 @@ def connect(request):
     Connection.objects.create(connection_id=connection_id)
     response = {
         "statusCode": 200,
-        "body": "Connected succesfully"
+        "body": "Connecte succesfully"
     }
     # return response
-    return JsonResponse('connected successfully', status=200, safe=False)
-
-
+    return JsonResponse('connect successfully', status=200, safe=False)
 @csrf_exempt
 def disconnect(request):
     body = _parse_body(request.body)
@@ -36,34 +30,30 @@ def disconnect(request):
     Connection.objects.get(connection_id=connection_id).delete()
     return JsonResponse('disconnect successfully', status=200, safe=False)
 
-
 def _send_to_connection(connection_id, data):
-    gatewayapi = boto3.client('apigatewaymanagementapi',
-                              endpoint_url="https://4hvqalbj8k.execute-api.us-east-2.amazonaws.com/test",
-                              region_name='us-east-2',
-                              aws_access_key_id='AKIAIXJ3QVQOFRF5DWKA',
-                              aws_secret_access_key='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')
+    gatewayapi = boto3.client('apigatewaymanagementapi', 
+    endpoint_url= "https://4hvqalbj8k.execute-api.us-east-2.amazonaws.com/test/",
+    region_name='us-east-2',
+    aws_access_key_id='AKIAIXJ3QVQOFRF5DWKA',
+    aws_secret_access_key= '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')
     return gatewayapi.post_to_connection(ConnectionId=connection_id, Data=json.dumps(data).encode('utf-8'))
-
-
 @csrf_exempt
 def send_message(request):
-    body = _parse_body(request.body)
-    ChatMessage.objects.create(
-        username=body['username'], message=body['message'], timestamp=body['timestamp'])
+    body = _parse_body(request.body) 
+    ChatMessage.objects.create(username=body['username'], message=body['message'], timestamp=body['timestamp'])
     connections = [i.connection_id for i in Connection.objects.all()]
-    data = {'messages': [body]}
+    data = {'messages':[body]}
     for connection in connections:
         _send_to_connection(connection, data)
     return JsonResponse('successfully sent', status=200, safe=False)
-
-
 @csrf_exempt
 def get_recent_messages(request):
     body = _parse_body(request.body)
     connection_id = body['connectionId']
     messages = ChatMessage.objects.all()
-    data = {'messages': [{'username': connection.username, 'message': connection.message,
-                          'timestamp': connection.timestamp} for message in messages]}
-    _send_to_connection(connection_id, data)
+    data = {'messages':[{'username':connection.username, 'message':connection.message,
+    'timestamp':connection.timestamp} for message in messages]}
+    _send_to_connection(connection_id, data )
     return JsonResponse('successfully sent', status=200)
+
+
